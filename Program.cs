9eaeadd429332f1b4798edb0a0178c6d1aa5eebc
malinabooy <1anejlbcuh4uk1@gmail.com>
2023-02-26 15:15:29 +0300

    FillResultArray

--- Program.cs
+++ Program.cs
@@ -9,8 +9,21 @@
     return count;
 }
 
+string[] FillResultArray(string[] array, int count)
+{
+    string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[result.Length - count] = array[i];
+            count--;
+        }
+    }
+    return result;
+}
 
 Console.WriteLine("Введите массив элементов через пробел:");
 string[] array = Console.ReadLine()!.Split(' ');
 int count = CountString(array);
-
+string[] result =  FillResultArray(array,count);
