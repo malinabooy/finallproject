b4a3ad99e6cc3b68756d84e7af032437567a40af
malinabooy <1anejlbcuh4uk1@gmail.com>
2023-02-26 15:11:06 +0300

    count

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int CountString(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+            count++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите массив элементов через пробел:");
+string[] array = Console.ReadLine()!.Split(' ');
+int count = CountString(array);
+
