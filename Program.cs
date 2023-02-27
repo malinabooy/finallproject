5070dc55b01fb8f27d2df056ba3849da36e09988
malinabooy <1anejlbcuh4uk1@gmail.com>
2023-02-27 11:48:27 +0300

    Добавил [ ] в принт массива

--- Program.cs
+++ Program.cs
@@ -25,12 +25,13 @@ string[] FillResultArray(string[] array, int count)
 
 void PrintArray(string[] array)
 {
+    Console.Write($"[");
     for (int i = 0; i < array.Length; i++)
     {
         if (i != array.Length - 1)
             Console.Write($"{array[i]}, ");
         else 
-            Console.Write($"{array[i]}.");
+            Console.Write($"{array[i]}]");
     }
     Console.WriteLine();
 }
